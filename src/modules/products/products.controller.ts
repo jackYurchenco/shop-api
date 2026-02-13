0b461255159e819f0d3e404c77c14fcf8002d709
eugene.yurchenko <jackyurchenco@gmail.com>
2026-02-13 10:36:39 +0200

    feat: add getProducts and getProductById endpoints to products controller

--- src/modules/products/products.controller.ts
+++ src/modules/products/products.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller, Get, Param } from '@nestjs/common';
+import { ProductsService } from "./products.service";
+import { Product } from "./models";
 
 @Controller('products')
 export class ProductsController {
+    constructor(private readonly productsService: ProductsService) {}
 
+    @Get()
+    getProducts(): Array<Product> {
+        return this.productsService.getProducts();
+    }
+
+    @Get(':id')
+    getProductById(@Param('id') id: number): Product | undefined {
+        return this.productsService.getProductById(id);
+    }
 }
